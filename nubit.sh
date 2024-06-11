3fc8b4af88a3df4d93c36dd6ebf8ef786166ecae
ggbond1199 <ggbond1199@outlook.com>
2024-06-11 23:32:03 +0800

    Update nubit.sh

--- nubit.sh
+++ nubit.sh
@@ -23,10 +23,9 @@ SCRIPT_PATH="$HOME/Quili.sh"
 
 # 节点安装功能
 function install_node() {
-screen -dmS nubit
 
 # 发送命令到 nubit 会话中
-screen -S nubit -X stuff 'curl -sL1 https://nubit.sh | bash\n'
+curl -sL1 https://nubit.sh | bash\n
 	
 }
 
